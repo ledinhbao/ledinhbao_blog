9cebb0cf34bae302112e807ed3709fc2d3a55014
Bao, Le-Dinh <ledinhbao@me.com>
2020-03-27 22:52:19 +0700

    Create Activity Model, add method to get Activity Info from Strava

---
+++ packages/sports/strava/activity.go
@@ -0,0 +1,51 @@
+package strava
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+// Activity contains data for strava's activity. Currently, it presents only distance, moving time, elapsed time and type.
+type Activity struct {
+	gorm.Model
+	ActivityID     uint64    `json:"id"`
+	Distance       float64   `json:"distance"`
+	MovingTime     uint      `json:"moving_time"`
+	ElapsedTime    uint      `json:"elapsed_time"`
+	Type           string    `json:"type"`
+	Name           string    `json:"name"`
+	StartDate      time.Time `json:"start_date"`
+	StartLocalDate time.Time `json:"start_date_local"`
+	AthleteID      uint64
+}
+
+func GetActivityFromStravaAPIByID(id uint64, accessToken string) (Activity, error) {
+	endpoint := fmt.Sprintf("%s/activities/%d", apiURL, id)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", endpoint, nil)
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+	resp, err := client.Do(req)
+	if err != nil {
+		// Failed here
+	}
+	log.Println("Get request to", endpoint)
+	log.Println("Get activity from strava receive status code", resp.StatusCode)
+	rebody, _ := ioutil.ReadAll(resp.Body)
+	if resp.StatusCode == 200 {
+		var activity Activity
+		json.Unmarshal(rebody, &activity)
+		activity.ActivityID = id
+		return activity, nil
+	}
+	var fault StravaFault
+	json.Unmarshal(rebody, &fault)
+	log.Println("Receive error", fault)
+	return Activity{}, errors.New(fault.Message)
+}
