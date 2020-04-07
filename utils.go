42e6c051f6e35584a169d3982914fb058640a395
Bao, Le-Dinh <ledinhbao@me.com>
2020-04-07 13:08:46 +0700

    add method to get user from session

--- utils.go
+++ utils.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/ledinhbao/blog/packages/models"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -26,4 +27,26 @@ func setSession(c *gin.Context, key string, value string) {
 	session := sessions.Default(c)
 	session.Set(key, value)
 	session.Save()
+}
+
+type noUserError struct{}
+type invalidUserError struct{}
+
+func (noUserError) Error() string {
+	return "No user exists in session"
+}
+func (invalidUserError) Error() string {
+	return "User's data is invalid"
+}
+
+func authUserFromSession(c *gin.Context) (models.User, error) {
+	s := sessions.Default(c)
+	user, ok := s.Get(authUser).(*models.User)
+	if !ok {
+		return models.User{}, noUserError{}
+	}
+	if user.ID <= 0 {
+		return models.User{}, invalidUserError{}
+	}
+	return *user, nil
 }
